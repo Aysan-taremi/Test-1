c0732b8c8757b349097fcd9f6cece566163bb946
Aysan Taremi <aysan.taremi@outlook.com>
2019-08-17 21:19:12 +0430

    Making some changes!

---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    {
+                        break;
+                    }
+
+                default:
+                    {
+                        e.Handled = true;
+                        break;
+                    }
+            }
+
+        }
+    }
+}
